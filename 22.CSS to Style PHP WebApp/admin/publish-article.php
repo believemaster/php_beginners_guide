fe8034d5926b41e7a5531a65339f1451f019fb1c
believemaster <believemasters@gmail.com>
2019-10-14 16:43:03 +0530

    Addign stylings and beautify the prpject using bootstrap

---
+++ 22.CSS to Style PHP WebApp/admin/publish-article.php
@@ -0,0 +1,15 @@
+<?php
+
+require '../include/init.php';
+
+Auth::requireLogin();
+
+$conn = require '../include/db.php';
+
+$article = Article::getByID($conn, $_POST['id']);
+
+$published_at = $article->publish($conn);
+
+?>
+
+<time><?= $published_at ?></time>
---
+++ 22.CSS to Style PHP WebApp/include/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/jquery.datetimepicker.min.css">
+		<title>NOTICE BOARD</title>
+	</head>
+	<body>
+		<div class="container">
+
+		<header>
+			<h1>COLLEGE NOTICE BOARD</h1>
+		</header>
+
+		<nav>
+			<ul class="nav">
+				<li class="nav-item"><a href="/index.php" class="nav-link">Home</a></li>
+
+				<?php if (Auth::isLoggedIn()): ?>
+
+					<li class="nav-item"><a href="admin/index.php" class="nav-link">Admin</a></li>
+
+					<li class="nav-item"><a href="./logout.php" class="nav-link">Logout</a></li>
+
+				<?php else: ?>
+
+						<li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+
+				<?php endif; ?>
+			</ul>
+		</nav>
+
+		<main>
+
+<!-- Note that these links are for temporary purpose using relative links when placing project in htdocs -->
+<!--In Production Use Relative paths. Not valid Here because no root directory specified (These Links might not work correctly)
+Use These:
+'/' for home
+'/logout' for logout
+'/login' for login
+'/admin/' for admin
+ -->
